073c8a2f2b5b3a4a82e4f2cd0d52a57e45305cbb
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-03-28 13:29:11 +0300

    Add files via upload

---
+++ Favorite animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace AnotherSinhThreadsExercises
+{
+    class Program
+    {
+        static object lockObject = new object();
+
+        static void Run()
+        {
+            lock (lockObject)
+            {
+                string [] favoritAnimls = Console.ReadLine().Split(' ').ToArray();
+
+                for (int i = 0; i < favoritAnimls.Length; i++)
+                {
+                    Console.WriteLine($"{favoritAnimls[i]}");
+                    Thread.Sleep(1000);
+                }
+            }
+           
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Run);
+            Thread thread1 = new Thread(Run);
+
+            thread.Start();
+            thread1.Start();
+
+        }
+    }
+}
+
+/*
+  1.	Създайте  обект за заключване locked , който може да се използва от множество нишки за синхронизиране на достъпа до текст .
+2.	Метод Run(), в който: 
+•	Блокирате обекта 
+•	Въвеждате текст в масив
+•	Извеждате текста дума по дума на отделни редове.
+3.	Свържете две дъщерни нишки с метода и стартирайте нишките
+
+ 
+ */
---
+++ Sinhronaizing Threads Exercise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace SinhronaizingThreadsExercises
+{
+    class Program
+    {
+        static object lockObject = new object();
+
+        static void Count() {
+            lock(lockObject)
+           //в and б lock (lockObject) 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Брояча е на{i}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread child1= new Thread(Count);
+            Thread child2 = new Thread(Count);
+            //в Thread child3 = new Thread(Count);
+            child1.Start();
+            child2.Start();
+            //в child3.Start();
+        }
+    }
+}
+
+/*
+ А)
+-Създайте  обект за заключване lockObject , който може да се използва от множество нишки за синхронизиране на достъпа до брояч.
+static object lockObject = new object();
+-Метод Count(), в който: 
+Блокирате обекта и стартирате   цикъл for за извеждане стойността на брояч от 1 до 10
+-Свържете две дъщерни нишки с метода и стартирайте нишките
+B) Решете задачата  с достъп на  нишкте до обект  без блокиране на обекта 
+C) Решете задачата  с достъп на  повече от две нишки  до обект  /със ,без блокиране на обекта/ 
+ */
