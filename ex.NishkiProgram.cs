40abde266ef32a2e3887fccd3685f93334aafcca
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-03-28 11:59:30 +0300

    Add files via upload

---
+++ ex.NishkiProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace ExNishki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваведете числоо!");
+            
+            Thread tr1 = new Thread(Factoriel);
+            tr1.Start();
+            Thread tr2 = new Thread(Dfactoriel);
+            tr2.Start();
+
+        }
+
+        public static void Factoriel()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Дъщерната нишка1 започна");
+            int result = 1;
+            for (int i = number; i > 1; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine("Нишката завърши1");
+            Console.WriteLine($"Резултата е {result}");
+            
+        }
+
+        public static void Dfactoriel()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Нишка 2 започма");
+            int result = 1;
+            for (int i = number; i <=1; i =-2)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("Нишка2 завърши");
+            Console.WriteLine($"{result}");
+        }
+    }
+}
