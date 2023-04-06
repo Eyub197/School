bda28a7dbb62750c0e6a6ecc0e3194a946d45fc2
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-04-06 10:45:07 +0300

    Add files via upload

---
+++ SkateboardsProject/Model/Data/Bearing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkateboardsProject.Data.Model
+{
+    class Bearing
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Brand))]
+        public ICollection<Brand> Brands { get; set; }
+
+        public Brand BrandId;
+        public string Name { get; set; }
+        public int Abec_ratiang { get; set; }
+        public string Bearing_material { get; set; }
+    }
+}
---
+++ SkateboardsProject/Model/Data/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkateboardsProject.Data.Model
+{
+    class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public string Country { get; set; }
+    }
+}
