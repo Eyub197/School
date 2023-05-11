027dcb94eef6efb215f699c9cc5ed2e9cdcf49d8
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-11 10:48:15 +0300

    Add files via upload

---
+++ SkateBoardProjectWin/Model/Data/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Data
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Skateboard> Skateboards { get; set; }
+    }
+}
