dc5ee45b4c348ab207616ac802072ff3b61745d0
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-18 10:47:25 +0300

    Add files via upload

---
+++ Idk/SkateBoards Display/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace SkateBoards_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBrands = new System.Windows.Forms.Button();
+            this.btnBrand = new System.Windows.Forms.Button();
+            this.btnBearing = new System.Windows.Forms.Button();
+            this.btnWheel = new System.Windows.Forms.Button();
+            this.btnDeck = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.lblSubTitle = new System.Windows.Forms.Label();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.panelDekstop = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Bisque;
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Controls.Add(this.btnWheel);
+            this.panelMenu.Controls.Add(this.btnDeck);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(142, 476);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(250)))), ((int)(((byte)(213)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.btnBrands);
+            this.panel1.Controls.Add(this.btnBrand);
+            this.panel1.Controls.Add(this.btnBearing);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 125);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(142, 351);
+            this.panel1.TabIndex = 3;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // btnBrands
+            // 
+            this.btnBrands.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.btnBrands.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBrands.FlatAppearance.BorderSize = 0;
+            this.btnBrands.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrands.Location = new System.Drawing.Point(0, 76);
+            this.btnBrands.Name = "btnBrands";
+            this.btnBrands.Size = new System.Drawing.Size(142, 38);
+            this.btnBrands.TabIndex = 3;
+            this.btnBrands.Text = "Skateboards";
+            this.btnBrands.UseVisualStyleBackColor = false;
+            this.btnBrands.UseWaitCursor = true;
+            // 
+            // btnBrand
+            // 
+            this.btnBrand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.btnBrand.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBrand.FlatAppearance.BorderSize = 0;
+            this.btnBrand.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrand.Location = new System.Drawing.Point(0, 38);
+            this.btnBrand.Name = "btnBrand";
+            this.btnBrand.Size = new System.Drawing.Size(142, 38);
+            this.btnBrand.TabIndex = 2;
+            this.btnBrand.Text = "Brands";
+            this.btnBrand.UseVisualStyleBackColor = false;
+            this.btnBrand.UseWaitCursor = true;
+            // 
+            // btnBearing
+            // 
+            this.btnBearing.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.btnBearing.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBearing.FlatAppearance.BorderSize = 0;
+            this.btnBearing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBearing.Location = new System.Drawing.Point(0, 0);
+            this.btnBearing.Name = "btnBearing";
+            this.btnBearing.Size = new System.Drawing.Size(142, 38);
+            this.btnBearing.TabIndex = 1;
+            this.btnBearing.Text = "Bearings";
+            this.btnBearing.UseVisualStyleBackColor = false;
+            this.btnBearing.UseWaitCursor = true;
+            // 
+            // btnWheel
+            // 
+            this.btnWheel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.btnWheel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnWheel.FlatAppearance.BorderSize = 0;
+            this.btnWheel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWheel.Location = new System.Drawing.Point(0, 87);
+            this.btnWheel.Name = "btnWheel";
+            this.btnWheel.Size = new System.Drawing.Size(142, 38);
+            this.btnWheel.TabIndex = 2;
+            this.btnWheel.Text = "Wheels";
+            this.btnWheel.UseVisualStyleBackColor = false;
+            this.btnWheel.UseWaitCursor = true;
+            // 
+            // btnDeck
+            // 
+            this.btnDeck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.btnDeck.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDeck.FlatAppearance.BorderSize = 0;
+            this.btnDeck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeck.Location = new System.Drawing.Point(0, 49);
+            this.btnDeck.Name = "btnDeck";
+            this.btnDeck.Size = new System.Drawing.Size(142, 38);
+            this.btnDeck.TabIndex = 1;
+            this.btnDeck.Text = "Decks";
+            this.btnDeck.UseVisualStyleBackColor = false;
+            this.btnDeck.UseWaitCursor = true;
+            this.btnDeck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(250)))), ((int)(((byte)(213)))));
+            this.panelLogo.Controls.Add(this.lblSubTitle);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(142, 49);
+            this.panelLogo.TabIndex = 0;
+            this.panelLogo.UseWaitCursor = true;
+            // 
+            // lblSubTitle
+            // 
+            this.lblSubTitle.AutoSize = true;
+            this.lblSubTitle.Location = new System.Drawing.Point(39, 21);
+            this.lblSubTitle.Name = "lblSubTitle";
+            this.lblSubTitle.Size = new System.Drawing.Size(56, 15);
+            this.lblSubTitle.TabIndex = 0;
+            this.lblSubTitle.Text = "DbTabels";
+            this.lblSubTitle.UseWaitCursor = true;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(150)))), ((int)(((byte)(218)))));
+            this.panelTitle.Controls.Add(this.lblHome);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(142, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(559, 49);
+            this.panelTitle.TabIndex = 1;
+            this.panelTitle.UseWaitCursor = true;
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHome.Location = new System.Drawing.Point(252, 9);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(69, 30);
+            this.lblHome.TabIndex = 0;
+            this.lblHome.Text = "Home";
+            this.lblHome.UseWaitCursor = true;
+            this.lblHome.Click += new System.EventHandler(this.lblHome_Click);
+            // 
+            // panelDekstop
+            // 
+            this.panelDekstop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panelDekstop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDekstop.Location = new System.Drawing.Point(142, 49);
+            this.panelDekstop.Name = "panelDekstop";
+            this.panelDekstop.Size = new System.Drawing.Size(559, 427);
+            this.panelDekstop.TabIndex = 2;
+            this.panelDekstop.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(210)))));
+            this.ClientSize = new System.Drawing.Size(701, 476);
+            this.Controls.Add(this.panelDekstop);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Skateboards Db Froms";
+            this.UseWaitCursor = true;
+            this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnDeck;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBrands;
+        private System.Windows.Forms.Button btnBrand;
+        private System.Windows.Forms.Button btnBearing;
+        private System.Windows.Forms.Button btnWheel;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Label lblSubTitle;
+        private System.Windows.Forms.Panel panelDekstop;
+    }
+}
+
