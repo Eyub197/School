621458c3da2f7abcb1ac7f6283dbf0ae84fee0b1
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-18 10:50:51 +0300

    Add files via upload

---
+++ Model/Data/Bearing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Data
+{
+    public class Bearing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Abec_ratiang { get; set; }
+        public string Bearing_material { get; set; }
+        public ICollection<Skateboard> Skateboards { get; set; }
+    }
+}
