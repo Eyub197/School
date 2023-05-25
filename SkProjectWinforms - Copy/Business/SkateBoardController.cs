d7f7a3e48032e844b9435cd35ea62c204faf70f9
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-25 18:49:49 +0300

    Add files via upload

---
+++ SkProjectWinforms - Copy/Business/SkateBoardController.cs
@@ -0,0 +1,66 @@
+﻿using Model;
+using Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    class SkateBoardController:IController<Skateboard>
+    {
+        private SkateboardsContext Context = new SkateboardsContext();
+
+        public void Add(Skateboard skateboard)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                Context.Skateboards.Add(skateboard);
+                Context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var skateboard = Context.Skateboards.Find(id);
+                if (skateboard != null)
+                {
+                    Context.Skateboards.Remove(skateboard);
+                    Context.SaveChanges();
+                }
+            }
+        }
+
+        public Skateboard Get(int id)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                return Context.Skateboards.Find(id);
+            }
+        }
+
+        public List<Skateboard> GetAll()
+        {
+            using (Context = new SkateboardsContext())
+            {
+                return Context.Skateboards.ToList();
+            }
+        }
+
+        public void Update(Skateboard skateboard)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var item = Context.Skateboards.Find(skateboard.Id);
+                if (item != null)
+                {
+                    Context.Entry(item).CurrentValues.SetValues(skateboard);
+                    Context.SaveChanges();
+                }
+            }
+        }
+    }
+}
