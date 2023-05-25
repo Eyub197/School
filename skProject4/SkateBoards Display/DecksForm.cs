917a8fa1f99ba9fd50bc872f4d44170230c0820c
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-25 18:54:13 +0300

    Add files via upload

---
+++ skProject4/SkateBoards Display/DecksForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkateBoards_Display
+{
+    public partial class DecksForm : Form
+    {
+        public DecksForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DecksForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
