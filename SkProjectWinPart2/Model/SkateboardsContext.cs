e47bffd10356b7b68787638f8c3d7b75756baf80
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-25 18:54:51 +0300

    Add files via upload

---
+++ SkProjectWinPart2/Model/SkateboardsContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Data;
+using System;
+
+namespace Model
+{
+    public class SkateboardsContext : DbContext
+    {
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Wheel> Wheels { get; set; }
+        public DbSet<Bearing> Bearings { get; set; }
+        public DbSet<Skateboard> Skateboards { get; set; }
+
+        public SkateboardsContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SkateBoardsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(connString);
+        }
+    }
+}
