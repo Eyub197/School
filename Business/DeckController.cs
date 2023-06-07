69b8199c6bcda8c271148b853fb4fdcd3611f0d2
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-06-07 17:52:49 +0300

    Add files via upload

---
+++ Business/DeckController.cs
@@ -0,0 +1,66 @@
+﻿using Model;
+using Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+     public  class DeckController:IController<Deck>
+    {
+        private SkateboardsContext Context = new SkateboardsContext();
+
+        public void Add(Deck deck)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                Context.Decks.Add(deck);
+                Context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var deck = Context.Decks.Find(id);
+                if (deck != null)
+                {
+                    Context.Decks.Remove(deck);
+                    Context.SaveChanges();
+                }
+            }
+        }
+
+        public Deck Get(int id)
+        {
+            using (var context = new SkateboardsContext())
+            {
+                return context.Decks.Find(id);
+            }
+        }
+
+        public List<Deck> GetAll()
+        {
+            using (Context = new SkateboardsContext())
+            {
+                return Context.Decks.ToList();
+            }
+        }
+
+        public void Update(Deck deck)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var item = Context.Decks.Find(deck.Id);
+                if (item != null)
+                {
+                    Context.Entry(item).CurrentValues.SetValues(deck);
+                    Context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ Business/WheelController.cs
@@ -0,0 +1,65 @@
+﻿using Model;
+using Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class WheelController:IController<Wheel>
+    {
+        private SkateboardsContext Context = new SkateboardsContext();
+        public void Add(Wheel wheel)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                Context.Wheels.Add(wheel);
+                Context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var wheel = Context.Wheels.Find(id);
+                if (wheel != null)
+                {
+                    Context.Wheels.Remove(wheel);
+                    Context.SaveChanges();
+                }
+            }
+        }
+
+        public Wheel Get(int id)
+        {
+            using (var context = new SkateboardsContext())
+            {
+                return context.Wheels.Find(id);
+            }
+        }
+
+        public List<Wheel> GetAll()
+        {
+            using (Context = new SkateboardsContext())
+            {
+                return Context.Wheels.ToList();
+            }
+        }
+
+        public void Update(Wheel wheel)
+        {
+            using (Context = new SkateboardsContext())
+            {
+                var item = Context.Decks.Find(wheel.Id);
+                if (item != null)
+                {
+                    Context.Entry(item).CurrentValues.SetValues(wheel);
+                    Context.SaveChanges();
+                }
+            }
+        }
+    }
+}
