bc56aeb425afe6b13d4199528b2985d6a527589a
Eyub <78847161+Eyub197@users.noreply.github.com>
2023-11-24 10:18:58 +0200

    Add files via upload

---
+++ KobinaotorikaEx/Kombinations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kombinations
+{
+    class Program
+    {
+        static int[] elements;
+        static int[] kombi;
+
+        public static void Combine(int index, int start)
+        {
+            if (index >= kombi.Length)
+                Console.WriteLine("(" + string.Join(", ", kombi) + ")");
+            else
+                for (int i = start; i < elements.Length; i++)
+                {
+                        var currentElement = elements[i];
+                        kombi[index] = currentElement;
+                        Combine(index + 1, i + 1);
+                }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            elements = new int[n];
+            Console.Write("k = ");
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+                elements[i] = i + 1;
+            kombi = new int[k];
+            Combine(0, 0);
+        }
+    }
+}
---
+++ KobinaotorikaEx/Varitations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Varitations
+{
+    class Program
+    {
+        static int[] elements;
+        static bool[] used;
+        static int[] vari;
+
+        public static void Variation(int index)
+        {
+            if (index >= vari.Length)
+                Console.WriteLine("(" + string.Join(", ", vari) + ")");
+
+            else
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        var currentElement = elements[i];
+                        used[i] = true;
+                        vari[index] = currentElement;
+                        Variation(index + 1);
+                        used[i] = false;
+                    }
+                }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            elements = new int[n];
+            Console.Write("k = ");
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+                elements[i] = i + 1;
+            used = new bool[elements.Length];
+            vari = new int[k];
+            Variation(0);
+        }
+    }
+}
