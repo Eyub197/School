ae3c2c24e6abb28cb2c125a4244341838c545c37
Eyub <78847161+Eyub197@users.noreply.github.com>
2024-02-09 13:19:48 +0200

    Add files via upload

---
+++ TreeFirstTouch/TreeFirstTouch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TreeFirstTouch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree1 = new Tree<int>(7,
+                new Tree<int>(15,
+                    new Tree<int>(12),
+                    new Tree<int>(2),
+                    new Tree<int>(22)),
+                new Tree<int>(13,
+                    new Tree<int>(5,
+                    new Tree<int>(23))),
+                new Tree<int>(24)
+                ); 
+
+            tree1.Print();
+        }
+    }
+}
---
+++ TreeFirstTouch/TreeFirstTouch/Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeFirstTouch
+{
+    public class Tree<T>
+    {
+        private T value;
+        private IList<Tree<T>> children;
+
+        public T Value { get;  set; }
+        public IList<Tree<T>> Children { get; private set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+            foreach( var chield in children)
+            {
+                this.Children.Add(chield);
+            }
+        }
+
+        public void Print(int indent = 0)
+        {
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(this.Value);
+            foreach (var chield in this.Children)
+            {
+                chield.Print(indent + 1);
+            }
+        }
+
+    }
+}
