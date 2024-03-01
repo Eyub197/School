4d259aac5df4818adb904686b4de22f1e658e368
Eyub <78847161+Eyub197@users.noreply.github.com>
2024-03-01 13:21:23 +0200

    Add files via upload

---
+++ TreeLoops/TreeLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(7,
+                new Tree<int>(19,
+                    new Tree<int>(1),
+                    new Tree<int>(12),
+                    new Tree<int>(21)),
+                new Tree<int>(21),
+                new Tree<int>(21,
+                    new Tree<int>(23),
+                    new Tree<int>(6)));  
+
+            tree.Print();
+            Console.WriteLine($"Koren : {tree.BFS().First()}");
+
+            List<int> treeValues = tree.BFS().ToList();
+            Console.WriteLine(string.Join(" ", treeValues));
+
+            Console.WriteLine($"Broi vasli : {treeValues.Count()}");
+
+            Console.WriteLine("Vavedete vazel za tarsene na sreshtane");
+            int number = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            foreach (var item in treeValues)
+            {         
+                if (item == number)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Broi vasli sas stoinost {number} se sresta {count} pati ");
+
+        }
+    }
+}
---
+++ TreeLoops/TreeLoops/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLoops
+{
+    public class Tree<T>
+    {
+        private T value;
+        private IList<Tree<T>> children;
+
+        public T Value { get; set; }
+        public Tree<T> Parent { get; set; }
+        public IList<Tree<T>> Children { get; set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+            }
+        }
+
+        public void Print( int indent = 0 )
+        {
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(this.Value);
+            foreach (var child in this.Children)
+                child.Print(indent + 1);
+        }
+
+        private void DFS(Tree<T> tree, List<T> order)
+        {
+            foreach (var child in this.Children)
+                this.DFS(child, order);
+            order.Add(tree.Value);
+        }
+        public IEnumerable<T> OrderDFS()
+        {
+            List<T> order = new List<T>();
+            this.DFS(this, order);
+            return order;
+        }
+
+        public IEnumerable<T> BFS()
+        {
+            List<T> order = new List<T>();
+            Queue<Tree<T>> visited = new Queue<Tree<T>>();
+            visited.Enqueue(this);
+            while (visited.Count > 0)
+            {
+                var next = visited.Dequeue();
+                order.Add(next.Value);
+                foreach (var child in next.Children)
+                    visited.Enqueue(child);
+            }
+            return order;
+        }
+        
+    }
+}
